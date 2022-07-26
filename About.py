a7dd0067abff1999fd98045063e9072473a7bed1
samattwood9 <59017719+samattwood9@users.noreply.github.com>
2022-07-26 11:10:28 +0100

    Init
    - include about page
    - include tokenization + regex page

---
+++ About.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown('# About')
+
+st.markdown('This dashboard will introduce you to the topic of natural langauge processing (NLP). Broadly speaking, NLP refers to technologies that aim to understand language using computational methods.')
+
+st.markdown('Use the sidebar to navigate the dashboard and explore topics. Going through the pages from top to bottom is advised, but it is up to you!')
+
