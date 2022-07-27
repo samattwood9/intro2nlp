9741017afac92713508c4007bbe9b9f82e690733
samattwood9 <59017719+samattwood9@users.noreply.github.com>
2022-07-27 09:45:07 +0100

    Update page 1 title / name

---
+++ pages/1_Regex_+_Tokenization.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import nltk
+nltk.download('punkt')
+
+st.markdown('# Regex + Tokenization')
+
+st.markdown("""
+    Tokenization and regular expressions (Regex) are essential to many NLP methods:
+    - Tokenization is the the process of turning a string of text or a document into smaller chunks.
+    - Regex allows you to search text for sequences and patterns.
+""")
+
+st.markdown("""
+    Tokenization may involve breaking a text up into sentences, or break a sentence up into words. Common forms
+    of tokenization like this can be implemented with ease using a library like `nltk` and it's utilities `sent_tokenize`
+    and `word_tokenize`. However, you can also use regex and `regexp_tokenize` to create custom patterns for tokenization.
+""")
+
+example_text = "This is some example text. Don't think it isn't! "
+
+st.markdown('## Sentence tokenization')
+
+with st.echo():
+    from nltk.tokenize import sent_tokenize
+
+    example_sentences = sent_tokenize(example_text)
+
+    st.write(example_sentences)
+
+st.markdown('## Word tokenization')
+
+with st.echo():
+    from nltk.tokenize import word_tokenize
+
+    example_words = word_tokenize(example_text)
+
+    st.write(example_words)
+
+st.markdown('## Advanced tokenization with regex')
+
+st.markdown("""
+    Use the text input widgets below to change the value of `example_text` and experiment with different regex patterns.
+""")
+
+example_text = st.text_area('Enter some example text:', 'This is some example text with a #tag.')
+
+input_pattern = st.text_input('Enter a regex pattern for tokenization:', '#\w+')
+
+with st.echo():
+    from nltk.tokenize import regexp_tokenize
+
+    # append the input pattern with an r so that it is read as regex, so #\w+ -> r"#\w+"
+    regex_pattern = r'%s'%(input_pattern) 
+
+    example_tokens = regexp_tokenize(example_text, regex_pattern)
+
+    st.write(example_tokens)
+
+    
+
+
